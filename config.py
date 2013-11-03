100dfa70c6bc76508a5f9806b884580316049833
Serban Carlogea <sherban.carlogea@gmail.com>
2013-11-03 19:28:13 +0100

    removed dead cells color

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 __author__ = 'Serban Carlogea'
 __email__  = 'sherban.carlogea@gmail.com'
 
-FPS = 1000
+FPS = 60
 
-DEAD_CELL_COLOR=(40, 40, 40)
 LIVE_CELL_COLOR=(0,255,0)
 GRID_LINE_COLOR=(140, 140, 140)
-GRID_BG_COLOR  =(255,255,255)
-
+GRID_BG_COLOR = (40, 40, 40)
 WINDOW_WIDTH   =800
 WINDOW_HEIGHT  =800
